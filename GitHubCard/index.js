c7262fed0c040c60546591c71296579cad8e1ed2
pbnj1492 <68866305+pbnj1492@users.noreply.github.com>
2022-09-19 20:09:35 -0700

    all tasks complete

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,19 @@
+const { default: axios } = require("axios");
+import "babel-polyfill";
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+// axios("https://api.github.com/users/jpabdou")
+//     .then(res => {
+//         console.log(res.data)
+//     })
+//     .catch(err => {
+//         console.error(err)
 
+//     })
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -28,7 +38,11 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ["tetondan",
+"dustinmyers",
+"justsml",
+"luishrd",
+"bigknell"];
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -50,6 +64,61 @@ const followersArray = [];
     </div>
 */
 
+async function cardMaker(usernames) {
+  const cardData = await axios(`https://api.github.com/users/${usernames}`)
+
+  const cardsWrapper= document.querySelector(".cards")
+
+  const div = document.createElement("div")
+  div.className ="card"
+  cardsWrapper.appendChild(div)
+
+  const cardImg = document.createElement("img")
+  cardImg.src = cardData.data.avatar_url
+  div.appendChild(cardImg)
+
+  const cardWrapper = document.createElement("div")
+  cardWrapper.className = "card-info"
+  div.appendChild(cardWrapper)
+
+  const name=document.createElement("h3")
+  name.className = "name"
+  name.textContent=cardData.data.name
+  cardWrapper.appendChild(name)
+
+  const username=document.createElement("p")
+  username.className = "username"
+  username.textContent=cardData.data.login
+  cardWrapper.appendChild(username)
+
+  const location = document.createElement("p")
+  location.textContent = `Location: ${cardData.data.location}`
+  cardWrapper.appendChild(location)
+  
+  const profText = document.createElement("p")
+  profText.textContent = "Profile: "
+  const profile = document.createElement("a")
+  profile.href = cardData.data.html_url
+  profile.textContent = cardData.data.html_url
+  profText.appendChild(profile)
+  cardWrapper.appendChild(profText)
+
+  const followers = document.createElement("p")
+  followers.textContent = `Followers: ${cardData.data.followers}`
+  cardWrapper.appendChild(followers)
+
+  const following = document.createElement("p")
+  following.textContent = `Following: ${cardData.data.following}`
+  cardWrapper.appendChild(following)
+
+  const bio = document.createElement("p")
+  bio.textContent = `Bio: ${cardData.data.bio}`
+  cardWrapper.appendChild(bio)
+}
+
+cardMaker("jpabdou")
+
+followersArray.forEach(element=> cardMaker(element))
 /*
   List of LS Instructors Github username's:
     tetondan
